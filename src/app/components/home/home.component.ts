a860186eafab5605f43eed1ddec4d03995f9bc62
tekillyu <hackingtekillyu@gmail.com>
2022-09-18 14:06:27 +0200

    Routes angular

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html'
+  selector: 'app-home',
+  templateUrl: './home.component.html'
 })
-export class FooterComponent implements OnInit {
+export class HomeComponent implements OnInit {
 
   constructor() { }
 
---
+++ src/app/components/noticias/noticias.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-noticias',
+  templateUrl: './noticias.component.html'
+})
+export class NoticiasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
