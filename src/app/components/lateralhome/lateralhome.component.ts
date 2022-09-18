da49fca6424837e0d38aae93bef7f8fe04107574
tekillyu <hackingtekillyu@gmail.com>
2022-09-18 17:01:25 +0200

    Add new components

---
+++ src/app/components/lateralhome/lateralhome.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lateralhome',
+  templateUrl: './lateralhome.component.html'
+})
+export class LateralhomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
