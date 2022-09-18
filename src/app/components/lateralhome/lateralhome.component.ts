af243ad72a199d4a92c4649173852ee427001917
tekillyu <hackingtekillyu@gmail.com>
2022-09-18 19:09:07 +0200

    Add new comp

--- src/app/components/lateralhome/lateralhome.component.ts
+++ src/app/components/lateralhome/lateralhome.component.ts
@@ -5,10 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './lateralhome.component.html'
 })
 export class LateralhomeComponent implements OnInit {
+  status: boolean = false;
+
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  
+  clickEvent(){
+      this.status = !this.status;       
   }
 
 }
